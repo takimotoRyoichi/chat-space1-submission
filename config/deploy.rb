17ef3cc5fa8f86ed6bf6e6f40a7a6dbb2994f9d6
takimotoRyoichi <cc.c2.npr@gmail.com>
2019-03-20 12:01:44 +0900

    Campfire

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application, "chatSpace"
+set :application, "chat-space1-submission"
 set :repo_url, "https://github.com/takimotoRyoichi/chat-space1-submission.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
+server '52.197.156.191', user: 'ec2-user', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
